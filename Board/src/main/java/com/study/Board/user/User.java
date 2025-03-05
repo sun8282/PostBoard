88b841459365d0557af7bc4f1674a9d6f555652e
SeungSoo Ha <109707901+sun8282@users.noreply.github.com>
2025-03-05 11:32:35 +0900

    add: User add

---
+++ Board/src/main/java/com/study/Board/user/User.java
@@ -0,0 +1,23 @@
+package com.study.Board.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String userId;
+}
