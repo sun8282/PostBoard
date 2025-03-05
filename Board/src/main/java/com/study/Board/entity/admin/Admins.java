ca91ec7ba2d35282c93f1ecafb6ef63c376822c7
SeungSoo Ha <109707901+sun8282@users.noreply.github.com>
2025-03-05 16:56:00 +0900

    add: admin entity

---
+++ Board/src/main/java/com/study/Board/entity/admin/Admins.java
@@ -0,0 +1,30 @@
+package com.study.Board.entity.admin;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Admins {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String adminId; // 관리자 ID (별도 지정)
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    public void encodePassword(PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(this.password);
+    }
+}
