bb46aae0d6b27d35800538e35fa5f826b819ca33
SeungSoo Ha <109707901+sun8282@users.noreply.github.com>
2025-03-05 17:03:36 +0900

    add: UserImages

---
+++ Board/src/main/java/com/study/Board/entity/users/UserImages.java
@@ -0,0 +1,24 @@
+package com.study.Board.entity.users;
+
+import com.study.Board.entity.posts.Post;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserImages {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String filePath; // 이미지 파일 경로
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User userId;
+}
