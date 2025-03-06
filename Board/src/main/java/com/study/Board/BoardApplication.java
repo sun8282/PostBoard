8f655d8203c9796a5fa39fab8890ea7aca05ca91
SeungSoo Ha <109707901+sun8282@users.noreply.github.com>
2025-03-06 16:27:43 +0900

    add: container에 띄운 db와 연결 성공

--- Board/src/main/java/com/study/Board/BoardApplication.java
+++ Board/src/main/java/com/study/Board/BoardApplication.java
@@ -1,5 +1,10 @@
 package com.study.Board;
 
+import com.study.Board.user.entity.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
